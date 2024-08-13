fcae4de11e255d9fb84ff637dd23590afda8adf5
kyibell <148383705+kyibell@users.noreply.github.com>
2024-08-13 12:08:26 -0500

    Implemented main function by creating a path based on user input.

--- desktop_cleaner.py
+++ desktop_cleaner.py
@@ -1,11 +1,30 @@
 import os
 import shutil
 
-
+def clean_folders(folder_path):
+    pass
 
 
 
 
 if __name__ == "__main__":
     print("Desktop Cleaner Script")
-    print("Enter what path you would like to clean up.")
+    print("Please enter your computer username:")
+    username = input()
+
+    print("Please enter the name of the path you want to clean up.")
+
+    folder_name = input()
+
+    folder_path  =  '/Users/' + username + '/' + folder_name
+
+    print(folder_path) 
+
+    if os.path.isdir(folder_path):
+        clean_folders(folder_path)
+    else:
+        print("Path not found, please ensure that the path is correct and please try again.")
+
+
+
+    
