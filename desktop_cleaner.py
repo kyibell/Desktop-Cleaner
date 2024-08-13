837c4c80380316bdf67f0fd577f81651468c9bbf
kyibell <148383705+kyibell@users.noreply.github.com>
2024-08-13 12:19:46 -0500

    Implemented the clean_folders function that loops through the directory given through input and creates sub folders based on the file extension

--- desktop_cleaner.py
+++ desktop_cleaner.py
@@ -1,8 +1,20 @@
 import os
 import shutil
 
-def clean_folders(folder_path):
+def create_subfolders(folder_path, subfolder_name):
     pass
+
+
+def clean_folders(folder_path):
+    # Loop through the directory files
+    for filename in os.listdir(folder_path):
+        #If the file is a valid file, take the subfolder name to sort it into different files
+        if os.path.isfile(os.path.join(folder_path, filename)):
+            # search the file for the '.' till the end of the str, and make it lowercase
+            file_extension = filename.split('.')[-1].lower()
+            # if there is a file extension, then make the subfolder name the exension + Files
+            if file_extension: 
+                subfolder_name = f"{file_extension.upper()} Files"
 
 
 
