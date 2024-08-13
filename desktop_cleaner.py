ff803d6b774159e6e8db32e620814225c75e2294
kyibell <148383705+kyibell@users.noreply.github.com>
2024-08-13 13:50:41 -0500

    Rewrote main function logic to instead of taking user input, it uses the os library to get the users current directory.

--- desktop_cleaner.py
+++ desktop_cleaner.py
@@ -1,18 +1,18 @@
 import os
 import shutil
 
-def create_subfolders(folder_path, subfolder_name):
-    pass
+def create_subfolders(current_directory, subfolder_name):
+    sub_folder_path = os.path.join(current_directory, subfolder_name)
 
 
-def clean_folders(folder_path):
+def clean_folders(current_directory):
     # Loop through the directory files
-    for filename in os.listdir(folder_path):
+    for filename in os.listdir(current_directory):
         #If the file is a valid file, take the subfolder name to sort it into different files
-        if os.path.isfile(os.path.join(folder_path, filename)):
+        if os.path.isfile(os.path.join(current_directory, filename)):
             # search the file for the '.' till the end of the str, and make it lowercase
             file_extension = filename.split('.')[-1].lower()
-            # if there is a file extension, then make the subfolder name the exension + Files
+            # if there is a file extension, then make the subfolder name the extension + Files
             if file_extension: 
                 subfolder_name = f"{file_extension.upper()} Files"
 
@@ -21,20 +21,19 @@ def clean_folders(folder_path):
 
 if __name__ == "__main__":
     print("Desktop Cleaner Script")
-    print("Please enter your computer username:")
-    username = input()
+    
+    current_directory = os.path.dirname(__file__)
+    print(current_directory)
 
-    print("Please enter the name of the path you want to clean up.")
+    print(f"You are running this script in: {current_directory}. Would you like to continue? Y/N")
+    response = input().lower() 
 
-    folder_name = input()
-    # Create the folder_path variable through the username + folder_name input
-    folder_path  =  '/Users/' + username + '/' + folder_name
-    # Check if the path is a directory
-    if os.path.isdir(folder_path):
-        clean_folders(folder_path)
+
+    if response == 'y' or 'yes':
+        print("Yes")
+       # clean_folders(current_directory)
     else:
-        print("Path not found, please ensure that the path is correct and please try again.")
-
+        print("Please run this script in the directory you want to clean.")
 
 
     
